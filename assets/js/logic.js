3e174bfd31952550004fde69e20dc71bc761c927
BenCottrell <bencottrell48@yahoo.com>
2022-10-09 20:45:16 -0400

    added the js for the logic behind the coding quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,108 +1,124 @@
 // variables to keep track of quiz state
-    let shuffledQuestions, currentQuestionIndex;
-    var seconds = 60;
-    // timerId
+let shuffledQuestions, currentQuestionIndex;
+var seconds = 60
+// timerId
 
 // variables to reference DOM elements
 var questionsEl = document.getElementById('questions');
 var startScreenEl = document.getElementById('start-screen');
 var endScreenEl = document.getElementById('end-screen');
-var feedbackEl = document.getElementById('feeback');
-
+var questionTitleEl = document.getElementById('question-title');
+var answerButtonsEl = document.getElementById('choices');
+var feedbackEl = document.getElementById('feedback');
+var counter = document.getElementById("timer");
 
 
 /* FUNCTION TO START THE QUIZ */
 function startQuiz() {
-    // hide start screen
+    // hides start screen and unhides questions screen
+    startScreenEl.classList.add('hide');
+    questionsEl.classList.remove('hide');
 
-    // un-hide questions section
+    // randomizes questions and creates a randomized array
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
 
-    // start timer
-
-    // show starting time
-
+    clockTick();
     getQuestions();
 }
 
 /* FUNCTION TO GET/SHOW EACH QUESTION */
 function getQuestions() {
-    // get current question object from array
-
-    // update title with current question
-
-    // clear out ant old question choices
-
-    // loop over choices
-        // FOR {
-            // create new button for each choice
-
-            // display on the page
-        
-        // }
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+function showQuestion(questions) {
+    if (currentQuestionIndex > 3) {
+        endScreenEl.classList.add('stop-time')
+        questionsEl.classList.add('hide');
+        endScreenEl.classList.remove('hide');
+        document.getElementById('final-score').innerHTML = counter.innerHTML
+    }
+    questionTitleEl.innerText = questions.title;
+    questions.choices.forEach(choices => {
+        const button = document.createElement('button')
+        button.innerText = choices.text
+        button.classList.add('button-style', 'answer-button')
+        if (choices.correct === true) {
+            button.classList.add('correct')
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
 }
 
-/* FUNCTION FOR CLICKING A QUESTION */
-function questionClick(event) {
-
-    // if the clicked element is not a choice button, do nothing.
-    if (something) {
-
+// removes last questions buttons
+function resetState() {
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
     }
+}
 
-    // check if user guessed wrong
-    if (something) {
-        // penalize time
-
-        // display new time on page
-
-        // give them feedback, letting them know it's wrong
+// shows review div, and takes away time if incorrect answer
+function selectAnswer(e) {
+    const selectedButton = e.target
+    if (selectedButton.classList.contains('correct')) {
+        feedbackEl.classList.remove('hide')
+        feedbackEl.innerHTML = 'Correct'
+        setTimeout(function () {
+            feedbackEl.innerHTML = '';
+        }, 1000);
     } else {
-        // give them feedback, letting them know it's right
+        seconds -= 10
+        feedbackEl.classList.remove('hide')
+        feedbackEl.innerHTML = 'Wrong'
+        setTimeout(function () {
+            feedbackEl.innerHTML = '';
+        }, 1000);
     }
-
-    // flash right/wrong feedback on page for a short period of time
-
-    // move to next question
-
-    // check if we've run out of questions
-        // if so, end the quiz
-        // else, get the next question    
-}
-
-/* FUNCTION TO END THE QUIZ */
-function quizEnd() {
-    // stop timer
-
-    // show end screen
-
-    // show final score
-
-    // hide questions section
+    currentQuestionIndex++
+    getQuestions()
 }
 
 /* FUNCTION FOR UPDATING THE TIME */
 function clockTick() {
-    // update time
+    seconds--;
+    counter.innerHTML =
+        (seconds < 10 ? "0" : "") + String(seconds);
+    if (endScreenEl.classList.contains('stop-time')) {
+        counter.innerHTML = document.getElementById('final-score').innerHTML
+    } else if (seconds > 0) {
+        setTimeout(clockTick, 1000);
 
-    // check if user ran out of time
+    } else {
+        counter.innerHTML = '00'
+        questionsEl.classList.add('hide');
+        endScreenEl.classList.remove('hide');
+        document.getElementById('final-score').innerHTML = counter.innerHTML
+    }
 }
 
+// saves relevant values, add to json local storage, nav to highscores page
 function saveHighscore() {
-    // get value of input box - for initials
+    var initials = document.getElementById('initials').value;
+    var finalScore = counter.innerHTML
+    if (initials == '') {
+        alert('Please input at least 1 character')
+        return null
+    }
+    var currentScore = { init: initials, score: finalScore };
+    var savedScores = JSON.parse(localStorage.getItem("savedScores"));
+    if (savedScores !== null) {
+        savedScores.push(currentScore);
+        localStorage.setItem("savedScores", JSON.stringify(savedScores));
+    } else {
+        savedScores = [currentScore];
+        localStorage.setItem("savedScores", JSON.stringify(savedScores));
+    }
 
-    // make sure value wasn't empty
-        // get saved scores from localstorage, or if not any, set to empty array
-
-        // format new score object for current user
-
-        // save to local storage
-
-        // redirect to next page
+    window.location.href = "./highscores.html";
 }
 
 /* CLICK EVENTS */
-    // user clicks button to submit initials
-
-    // user clicks button to start quiz
-
-    // user clicks on element containing choices
+document.getElementById('submit-button').addEventListener('click', saveHighscore)
+document.getElementById('start-button').addEventListener('click', startQuiz)
