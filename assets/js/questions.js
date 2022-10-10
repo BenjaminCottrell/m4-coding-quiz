a15945c4a3dd754255cf3e1955d6d9d60dc79652
BenCottrell <bencottrell48@yahoo.com>
2022-10-10 03:45:00 -0400

    reformatted questions.js array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,51 +3,46 @@ var questions = [
     {
         title: 'Commonly used data types DO NOT include:',
         choices: [
-            'strings',
-            'booleans',
-            'alerts',
-            'numbers'
-        ],
-        answer: 'alerts'
+            { text: 'strings', correct: false},
+            { text: 'booleans', correct: false},
+            { text: 'alerts', correct: true},
+            { text: 'numbers', correct: false}
+        ]
     },
     {
         title: 'The condition in an if / else statement is enclosed within ___.',
         choices: [
-            'quotes',
-            'curly brackets',
-            'parentheses',
-            'square brackets'
-        ],
-        answer: 'parentheses'
+            { text: 'quotes', correct: false},
+            { text: 'curly brackets', correct: false},
+            { text: 'parentheses', correct: true},
+            { text: 'square brackets', correct: false}
+        ]
     },
     {
         title: 'Arrays in JavaScript can be used to store ___.',
         choices: [
-            'numbers and strings',
-            'other arrays',
-            'booleans',
-            'all of the above'
+            { text: 'numbers and strings', correct: false},
+            { text: 'other arrays', correct: false},
+            { text: 'booleans', correct: false},
+            { text: 'all of the above', correct: true}
         ],
-        answer: 'all of the above'
     },
     {
         title: 'String values must be enclosed within ___ when being assigned to variables.',
         choices: [
-            'commas',
-            'curly brackets',
-            'quotes',
-            'parentheses'
-        ],
-        answer: 'quotes'
+            { text: 'commas', correct: false},
+            { text: 'curly brackets', correct: false},
+            { text: 'quotes', correct: true},
+            { text: 'parentheses', correct: false}
+        ]
     },
     {
         title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
         choices: [
-            'JavaScript',
-            'terminal / bash',
-            'for loops',
-            'console.log'
-        ],
-        answer: 'console.log'
+            { text: 'JavaScript', correct: false},
+            { text: 'terminal / bash', correct: false},
+            { text: 'for loops', correct: false},
+            { text: 'console.log', correct: true}
+        ]
     }
 ];
